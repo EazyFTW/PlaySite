ec5b20b199bad3b5166410a4dd8aa80090bc9ffd
lucas lee <lixin9311@gmail.com>
2022-07-30 11:03:10 +0900

    use hash router to avoid 404

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import PlayCover from './PlayCover';
 import './index.css';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <PlayCover />
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 );
